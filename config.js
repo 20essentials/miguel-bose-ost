78fbaca78175bc42d5d32144681d4e1bbd82e062
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:51:21 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MIGUEL BOSÉ';
+export let nameSongs = [
+	"HORIZONTE DE LAS ESTRELLAS", /*1*/
+	"AMANTE BANDIDO", /*2*/
+	"MORENA MÍA", /*3*/
+	"COMO UN LOBO", /*4*/
+	"NENA", /*5*/
+	"CREO EN TI", /*6*/
+	"AMIGA", /*7*/
+	"MORIR DE AMOR", /*8*/
+	"TE AMARÉ", /*9*/
+	"LINDA", /*10*/
+	"MI LIBERTAD", /*11*/
+	"ERES TODO PARA MI ", /*12*/
+	"LENTO", /*13*/
+	"SOUTH OF THE SAHARA", /*14*/
+	"Y FUE", /*15*/
+	"BRAVO MUCHACHOS", /*16*/
+	"SON AMIGOS", /*17*/
+	"DON DIABLO", /*18*/
+	"LA CHULA", /*19*/
+	"MÁRCHATE YA", /*20*/
+]
